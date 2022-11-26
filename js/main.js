b23edd98fd0153f7a16a155ac21b1d18c9cda67d
Madelen Foss <113188008+madelenfoss@users.noreply.github.com>
2022-11-26 19:58:14 +0100

    Imported js modules to main.js

--- js/main.js
+++ js/main.js
@@ -1,2 +1,12 @@
 /* importere alle js-filene hit, 
 så linke til denne i index.html */
+
+import Header from './modules/header.js';
+import Slideshow from './modules/slideshow.js';
+import Ageconverter from './modules/ageconverter.js';
+
+Header();
+Slideshow();
+Ageconverter();
+
+
