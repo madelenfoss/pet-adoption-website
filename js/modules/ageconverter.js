706f5f904a45d19ce3959f4f77cd7fb2fc5c0c86
Madelen Foss <113188008+madelenfoss@users.noreply.github.com>
2022-11-27 19:32:28 +0100

    Added about page + css and more age converter js

--- js/modules/ageconverter.js
+++ js/modules/ageconverter.js
@@ -7,29 +7,41 @@
  * 20 bunny years
  */
 
- const humanYearsInput = document.querySelector('#human');
-//  const dogAge = document.querySelector('.converter__output-dog');
-//  const catAge = document.querySelector('.converter__output-cat');
-//  const budgieAge = document.querySelector('.converter__output-budgie');
-//  const parrotAge = document.querySelector('.converter__output-parrot');
-//  const bunnyAge = document.querySelector('.converter__output-cat');
-
-//  function humanToPetAge(val) {
-//  	dogAge.value = (val*7);
-//  	catAge.value = (val*8);
-//  	budgieAge.value = (val*7);
-//  	parrotAge.value = (val*4);
-//  	bunnyAge.value = (val*10);
+ 
+// const humanYearsInput = document.querySelector('#human');
+// const dogAge = document.querySelector('.converter__output-dog');
+// const catAge = document.querySelector('.converter__output-cat');
+// const budgieAge = document.querySelector('.converter__output-budgie');
+// const parrotAge = document.querySelector('.converter__output-parrot');
+// const bunnyAge = document.querySelector('.converter__output-cat');
+ 
+// function humanToPetAge(val) {
+// 	 dogAge.value.innerHTML = val*7;
+// 	 catAge.value.innerHTML = val*8;
+// 	 budgieAge.value.innerHTML = val*7;
+// 	 parrotAge.value.innerHTML = val*4;
+// 	 bunnyAge.value.innerHTML = val*10;
 //  }
 
- humanYearsInput.addEventListener('oninput', () => {
-	document.querySelector('converter__output-dog').innerHTML = humanYearsInput * 7;
-	document.querySelector('converter__output-cat').innerHTML = humanYearsInput * 7;
-	document.querySelector('converter__output-budgie').innerHTML = humanYearsInput * 7;
-	document.querySelector('converter__output-parrot').innerHTML = humanYearsInput * 7;
-	document.querySelector('converter__output-bunny').innerHTML = humanYearsInput * 7;
+
+const humanYearsInput = document.querySelector('#human');
+const dogAge = document.querySelector('.converter__output-dog');
+const catAge = document.querySelector('.converter__output-cat');
+const budgieAge = document.querySelector('.converter__output-budgie');
+const parrotAge = document.querySelector('.converter__output-parrot');
+const bunnyAge = document.querySelector('.converter__output-cat');
+
+ humanYearsInput.addEventListener('humanYearsInput', () => {
+	document.querySelector('.converter__output-dog').innerHTML = humanYearsInput * 7;
+	document.querySelector('.converter__output-cat').innerHTML = humanYearsInput * 7;
+	document.querySelector('.converter__output-budgie').innerHTML = humanYearsInput * 7;
+	document.querySelector('.converter__output-parrot').innerHTML = humanYearsInput * 7;
+	document.querySelector('.converter__output-bunny').innerHTML = humanYearsInput * 7;
 
  });
+
+
+
 
 
 
