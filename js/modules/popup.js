311f12d800de0aa415da8f428e416a3598f76fa4
Madelen Foss <113188008+madelenfoss@users.noreply.github.com>
2022-12-01 22:02:33 +0100

    Created dog popup card

--- js/modules/popup.js
+++ js/modules/popup.js
@@ -1,16 +1,17 @@
 export default function Popup() {
 
-	const overLay = document.querySelector('#contentbox__popup');
+	const background = document.querySelector('#contentbox__popup');
 
 	document.querySelector('#contentbox__openpopup').addEventListener('click', () => {
-		overLay.style.display = "block";
+		background.style.display = "block";
 	}) 
 	
 	document.querySelector('#contentbox__closepopup').addEventListener('click', () => {
-		overLay.style.display = "none";
+		background.style.display = "none";
 	}) 
 	
-
+	const arneWoof = document.querySelector('arne-woof');
+	
 
 
 
