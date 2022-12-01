a9128984265aed3cce9d8f6271d96afa2bf02bd4
Madelen Foss <113188008+madelenfoss@users.noreply.github.com>
2022-12-01 23:30:39 +0100

    Added sound to buttons

--- js/modules/popup.js
+++ js/modules/popup.js
@@ -10,8 +10,28 @@ export default function Popup() {
 		background.style.display = "none";
 	}) 
 	
-	const arneWoof = document.querySelector('arne-woof');
+	/* Animal sounds */ 
+
+	// This one works, but turns all buttons into sound buttons:
+	 const audio = new Audio('./assets/sounds/dog_barking-6296.mp3');
+	 const buttons = document.querySelectorAll('button');
+
+	  buttons.forEach(button => {
+	  	button.addEventListener('click', () => {
+	  		audio.play();
+	  	});
+	  });
 	
+
+	// This one does not work (yet)
+ 	//const audio = new Audio('./assets/sounds/dog_barking-6296.mp3');
+
+  	//document.querySelector('arnewoof').addEventListener('click', () => {
+  	//	audio.play();
+  	//})
+
+
+
 
 
 
