e7a13caf43de88bfc67e77586eef287c64533a29
Madelen Foss <113188008+madelenfoss@users.noreply.github.com>
2022-12-02 16:32:38 +0100

    Added more animal sounds, created new dog popup

--- js/modules/popup.js
+++ js/modules/popup.js
@@ -1,38 +1,71 @@
 export default function Popup() {
+	
+	// Arne dog popup card
+	const backgroundArne = document.querySelector('#contentbox__popup-1');
 
-	const background = document.querySelector('#contentbox__popup');
-
-	document.querySelector('#contentbox__openpopup').addEventListener('click', () => {
-		background.style.display = "block";
+	document.querySelector('#contentbox__openpopup-1').addEventListener('click', () => {
+		backgroundArne.style.display = "block";
 	}) 
 	
-	document.querySelector('#contentbox__closepopup').addEventListener('click', () => {
-		background.style.display = "none";
+	document.querySelector('#contentbox__closepopup-1').addEventListener('click', () => {
+		backgroundArne.style.display = "none";
+	}) 
+
+	// Berta dog popup card
+	const backgroundBerta = document.querySelector('#contentbox__popup-2');
+
+	document.querySelector('#contentbox__openpopup-2').addEventListener('click', () => {
+		backgroundBerta.style.display = "block";
 	}) 
 	
-	/* Animal sounds */ 
+	document.querySelector('#contentbox__closepopup-2').addEventListener('click', () => {
+		backgroundBerta.style.display = "none";
+	}) 
+
+	
+
+
+	
+	/* Animal sounds - See Arne the dog for example */ 
 
 	// This one works, but turns all buttons into sound buttons:
-	 const audio = new Audio('./assets/sounds/dog_barking-6296.mp3');
-	 const buttons = document.querySelectorAll('button');
 
-	  buttons.forEach(button => {
-	  	button.addEventListener('click', () => {
-	  		audio.play();
-	  	});
-	  });
+	//  const audio = new Audio('./assets/sounds/dog_barking-6296.mp3');
+	//  const buttons = document.querySelectorAll('button');
+
+	//   buttons.forEach(button => {
+	//   	button.addEventListener('click', () => {
+	//   		audio.play();
+	//   	});
+	//   });
 	
 
-	// This one does not work (yet)
- 	//const audio = new Audio('./assets/sounds/dog_barking-6296.mp3');
+	/* Dog sounds */
+ 	const arneAudio = new Audio('./assets/sounds/dog_barking-6296.mp3');
 
-  	//document.querySelector('arnewoof').addEventListener('click', () => {
-  	//	audio.play();
-  	//})
+	if (arneAudio) {
+  		document.querySelector('.arnewoof').addEventListener('click', () => {
+  			arneAudio.play();
+  	});
+ }
 
 
+ 	const bertaAudio = new Audio('./assets/sounds/tubby-bark-1-48313.mp3');
 
+	if (bertaAudio) {
+		document.querySelector('.bertawoof').addEventListener('click', () => {
+			bertaAudio.play();
+	})
+}
 
+ 	/* Cat sounds */
+	const katlaAudio = new Audio('./assets/sounds/cat-meow-14536.mp3');
+
+	if (katlaAudio) {
+		document.querySelector('.katlameow').addEventListener('click', () => {
+			katlaAudio.play();
+		})
+	}
 
 
 
